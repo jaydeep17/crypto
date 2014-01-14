c01dae685abfe414892d1c8d672b24c059000e67
Jaydeep Solanki <jaydp17@gmail.com>
2014-01-14 19:42:21 +0530

    caesar smart bruteforce

--- src/caesarCipher.py
+++ src/caesarCipher.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 def encrypt(text, key):
     txt = list(text)
     for i in range(len(txt)):
@@ -10,3 +12,44 @@ def decrypt(cipherText, key):
     for i in range(len(txt)):
         txt[i] = chr(ord(txt[i]) - key)
     return ''.join(txt)
+
+
+def bruteForce(cipherText):
+    """
+    @rtype : [ (key, plaintext) ]
+    @param cipherText: cipher text
+    @return: a list of plainTexts
+    """
+    lst = []
+    for i in range(26):
+        plainText = decrypt(cipherText, i)
+        lst.append((i, plainText))
+    return lst
+
+
+def smartBruteForce(cipherText, thresh):
+    """
+    Uses NLTK to detect if the resulting text from brute force
+    is a valid english text or not. If the ratio of (valid-english-words/total-words)
+    is >= thresh, it assumes that it is a valid result
+
+    @rtype : [ (key, plaintext, ratio) ]
+    @param cipherText: cipher text
+    @param thresh: percentage of words in resulting plain text to be valid english words [ should be between (0...1) ]
+    @return: a filtered list of results that pass the threshold
+    """
+    import nltk
+    from nltk.corpus import wordnet
+
+    lst = []
+    for i in range(26):
+        plainText = decrypt(cipherText, i)
+        tokens = nltk.word_tokenize(plainText)
+        cnt = 0
+        for token in tokens:
+            if wordnet.synsets(token):
+                cnt += 1
+        ratio = cnt/len(tokens)
+        if ratio >= thresh:
+            lst.append((i, plainText, ratio))
+    return sorted(lst, key=itemgetter(2), reverse=True)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from src import caesarCipher
-from src import freqAnalysis
-
 
 def main():
-    text = 'iq ifcc vqqr fb rdq vfllcq na rdq cfjwhwz hr bnnb hcc hwwhbsqvqbre hwq vhlq'
-    arr = freqAnalysis.analyse(text)
-    print(arr)
-    print(freqAnalysis.englishLetterFreq())
+    text = 'Hello world is the best program'
+    key = 10
+    cipherText = caesarCipher.encrypt(text, key)
+    print(caesarCipher.bruteForce(cipherText))
+    print('smart brute force')
+    sb = caesarCipher.smartBruteForce(cipherText, 0.5)
+    print(sb)
 
 
 if __name__ == "__main__":
